75c542d869b652146f035124aa5d14845e808c77
midhunkrishna26 <midhunkrishna3333@gmail.com>
2024-08-06 14:59:54 +0530

    intialcommit

---
+++ l2/Ope.java
@@ -0,0 +1,17 @@
+package l2;
+
+public class Ope 
+{
+ public static void main(String[] args)
+        {
+	     System.out.println(2+3);
+	     System.out.println(5-2);
+	     System.out.println(5/2);
+	     System.out.println(5*2);
+	     System.out.println(5%2);
+	     System.out.println(5|7);
+	     System.out.println(5>>2);
+	     int a=36;
+	     System.out.println(a);
+        }
+}
---
+++ sample1/src/l2/Star.java
@@ -0,0 +1,18 @@
+package l2;
+
+public class Star
+{
+	public static void main(String[] args)
+	{
+		System.out.println("     * ");
+		System.out.println("    * *");
+		System.out.println("   * * *");
+		System.out.println("  * * * *");
+		System.out.println(" * * * * *");
+		System.out.println("* * * * * *");
+
+
+	}
+ 
+}
+
